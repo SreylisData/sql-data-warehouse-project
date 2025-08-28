48fe7be88d8df721bb84f58c27113540aed347f6
SreylisData <sreylis.rin@gmail.com>
2025-08-28 00:12:38 -0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -24,7 +24,7 @@ CREATE TABLE bronze.crm_cust_info (
 DROP TEMPORARY TABLE IF EXISTS bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info (
-	  prd_id INT,
+	prd_id INT,
     prd_key VARCHAR(50),
     prd_nm VARCHAR(50),
     prd_cost INT,
@@ -36,7 +36,7 @@ CREATE TABLE bronze.crm_prd_info (
 DROP TEMPORARY TABLE IF EXISTS bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details (
-	  sls_ord_num VARCHAR(50),
+	sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
     sls_order_dt INT,
@@ -50,7 +50,7 @@ CREATE TABLE bronze.crm_sales_details (
 DROP TEMPORARY TABLE IF EXISTS bronze.erp_cust_az12;
 
 CREATE TABLE bronze.erp_cust_az12 (
-	  cid VARCHAR(50),
+	cid VARCHAR(50),
     bdate DATE,
     gen VARCHAR(50)
 );
@@ -58,14 +58,14 @@ CREATE TABLE bronze.erp_cust_az12 (
 DROP TEMPORARY TABLE IF EXISTS bronze.erp_loc_a101;
 
 CREATE TABLE bronze.erp_loc_a101 (
-	  cid VARCHAR(50),
+	cid VARCHAR(50),
     cntry VARCHAR(50)
 );
 
 DROP TEMPORARY TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-	  id VARCHAR(50),
+	id VARCHAR(50),
     cat VARCHAR(50),
     subcat VARCHAR(50),
     maintenance VARCHAR(50)
